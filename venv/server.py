56091d61ab617ed8f74695fe59543b56dfcee1f0
NekoCarreraDesigns <denvermaas@gmail.com>
2022-08-17 15:16:48 -0600

    edited server.py

--- venv/server.py
+++ venv/server.py
@@ -15,7 +15,7 @@ app.config["SQL_TRACK_MODIFICATIONS"] = False
 event.listens_for(Engine, "connect")
 
 
-def set_sqlite_pragma(dbapi_connection, connection_record):
+def _set_sqlite_pragma(dbapi_connection, connection_record):
     if isinstance(dbapi_connection, SQLite3Connection):
         cursor = dbapi_connection.cursor()
         cursor.execute("PRAGMA foreign_keys=ON;")
