093ae005cd1f97fa8b8679053536e82c54539efe
NekoCarreraDesigns <denvermaas@gmail.com>
2022-08-25 13:53:24 -0600

    added logic to creae blog post function

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import linked_list
+import hash_table
 
 # app
 app = Flask(__name__)
@@ -130,9 +131,28 @@ def delete_user(user_id):
     return jsonify({"message": "User deleted"}), 200
 
 
-@app.route("/user/<user_id>", methods=["POST"])
+@app.route("/blog_post/<user_id>", methods=["POST"])
 def create_blog_post(user_id):
-    pass
+    data = request.get_json()
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        return jsonify({"message": "User does not exist!"}), 400
+
+    ht = hash_table.HashTable(10)
+    ht.add_key_value("title", data["title"])
+    ht.add_key_value("body", data["body"])
+    ht.add_key_value("date", now)
+    ht.add_key_value("user_id", user_id)
+
+    new_blog_post = BlogPost(
+        title=ht.get_value("title"),
+        body=ht.get_value("body"),
+        date=ht.get_value("date"),
+        user_id=ht.get_value("user_id")
+    )
+    db.session.add(new_blog_post)
+    db.session.commit()
+    return jsonify({"message": "Blog post created!"}), 200
 
 
 @app.route("/user/<blog_post_id>", methods=["GET"])
