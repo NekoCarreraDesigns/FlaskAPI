912beafe740e5b121ab068fb95e4d139fb3180ee
NekoCarreraDesigns <denvermaas@gmail.com>
2022-08-26 15:07:32 -0600

    added logic to blog post search function

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import linked_list
 import hash_table
+import binary_search_tree
+import random
 
 # app
 app = Flask(__name__)
@@ -155,9 +157,28 @@ def create_blog_post(user_id):
     return jsonify({"message": "Blog post created!"}), 200
 
 
-@app.route("/user/<blog_post_id>", methods=["GET"])
+@app.route("/blog_post/<blog_post_id>", methods=["GET"])
 def get_all_blog_posts(blog_post_id):
-    pass
+    blog_posts = BlogPost.query.all()
+    random.shuffle(blog_posts)
+
+    bst = binary_search_tree.BinarySearchTree()
+
+    for post in blog_posts:
+        bst.insert({
+            "id": post.id,
+            "title": post.title,
+            "body": post.body,
+            "user_id": post.user_id,
+
+        })
+
+    post = bst.search(blog_post_id)
+
+    if not post:
+        return jsonify({"message": "post not found"})
+
+    return jsonify(post)
 
 
 @app.route("/user/<blog_post_id>", methods=["GET"])
