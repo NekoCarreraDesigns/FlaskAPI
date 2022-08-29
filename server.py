6dced96438f12e239aa88b6b14332f4ebde2c4bf
NekoCarreraDesigns <denvermaas@gmail.com>
2022-08-29 15:25:24 -0600

    created delete function using stack algorithm

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ import linked_list
 import hash_table
 import binary_search_tree
 import custom_queue
+import stack
 import random
 
 # app
@@ -214,9 +215,21 @@ def get_numeric_post_body():
     return jsonify(return_list)
 
 
-@app.route("/user/<blog_post_id>", methods=["DELETE"])
-def delete_blog_post(blog_post_id):
-    pass
+@app.route("/blog_post/delete_last_10", methods=["DELETE"])
+def delete_last_10():
+    blog_posts = BlogPost.query.all()
+
+    s = stack.Stack()
+
+    for post in blog_posts:
+        s.push(post)
+
+    for _ in range(10):
+        post_to_delete = s.pop()
+        db.session.delete(post_to_delete.data)
+        db.session.commit()
+
+    return jsonify({"message": "posts deleted"}), 200
 
 
 if __name__ == "__main__":
